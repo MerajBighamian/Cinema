179d77eee9bfcce196f38091379ea2787cdcf4d9
Meraj <57762033+MerajBighamian@users.noreply.github.com>
2022-05-02 15:01:37 +0430

    refactor model

--- accounts/models.py
+++ accounts/models.py
@@ -12,7 +12,7 @@ class Profile(models.Model):
         verbose_name = 'نمایه کاربری'
         verbose_name_plural = "نمایه های کاربری"
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="حساب کاربری")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="حساب کاربری") # one to one field for connect profile model to user model
     # important fields that are sorted in User Model:
     # first_name, last_name, email , data_joined
     mobile = models.CharField('تلفن همراه', max_length=11)
@@ -26,22 +26,22 @@ class Profile(models.Model):
 
     # fields related to tickets
 
-    balance = models.IntegerField('اعتبار', default=0)
+    balance = models.IntegerField('اعتبار', default=0) # integer filed for hold balance of user profile
 
     def __str__(self):
-        return self.user.get_full_name()
+        return self.user.get_full_name() # return full name of user
 
     def get_balance_display(self):
-        return '{} تومان'.format(self.balance)
+        return '{} تومان'.format(self.balance) # display balance of user with toman format
 
     # behaviors
-    def deposit(self, amount):
+    def deposit(self, amount): # deposit balance of user profile
         self.balance += amount
         self.save()
 
-    def spend(self, amount):
-        if self.balance < amount:
+    def spend(self, amount): # spend balance of user profile
+        if self.balance < amount: # check amount is lowter form balance money
             return False
         self.balance -= amount
-        self.save()
+        self.save() # save spended balance in model
         return True
