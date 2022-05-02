a260e6d79538b61842766441e451818dca01c314
Meraj <57762033+MerajBighamian@users.noreply.github.com>
2022-05-02 14:32:29 +0430

    refactor urls routing

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-app_name = 'accounts'
+
+app_name = 'accounts' # app_name for clean url routing
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('profile/details', views.profile_details, name='profile_details')
+    path('login/', views.login_view, name='login'), # define url for login view
+    path('logout/', views.logout_view, name='logout'), # define url for logout view
+    path('profile/details', views.profile_details, name='profile_details') # define url for profile_details view
 ]
