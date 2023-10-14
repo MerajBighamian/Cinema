c3ef2728abfe836a1cd231ecb2b21dfa2b63bcbd
Meraj <57762033+MerajBighamian@users.noreply.github.com>
2023-10-14 12:27:01 +0330

    Update apps.py

--- ticketing/apps.py
+++ ticketing/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class TicketingConfig(AppConfig):
     name = 'ticketing'
+    # this is verbose name for show this name on panel
     verbose_name = 'مدیریت فروش بلیط'
